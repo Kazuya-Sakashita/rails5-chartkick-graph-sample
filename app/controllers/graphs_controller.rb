bbbeef9a8bdf9998990554df567351881e4edf48
Kazuya-Sakashita <kz0508@gmail.com>
2021-05-25 21:04:01 +0900

    不要なコード削除

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,8 +1,6 @@
 class GraphsController < ApplicationController
   def index
       @graph = Graph.group(:time).average(:congestion_level)
-      @graph1 = Graph.group(:time).average(:congestion_level)
-      @graph2 = Graph.group(:time).average(:congestion_level)
 
   end
 end

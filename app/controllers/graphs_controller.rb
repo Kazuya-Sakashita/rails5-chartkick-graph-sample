5ab8046ef2d81b2caaccfee70aeb75ab1ba2ec99
Kazuya-Sakashita <kz0508@gmail.com>
2021-05-25 20:56:16 +0900

    カラー、大きさ調整お試し

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,5 +1,8 @@
 class GraphsController < ApplicationController
   def index
       @graph = Graph.group(:time).average(:congestion_level)
+      @graph1 = Graph.group(:time).average(:congestion_level)
+      @graph2 = Graph.group(:time).average(:congestion_level)
+
   end
 end
